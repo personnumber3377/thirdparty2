c7ae440a4ab8ae587bd5ff911b6932942bb7b871
personnumber3377 <sontapaa.jokulainen@gmail.com>
2025-09-18 00:13:03 +0300

    grwgre

---
+++ aaa.sh
@@ -0,0 +1,7 @@
+echo "# thirdparty2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/personnumber3377/thirdparty2.git
+git push -u origin main
